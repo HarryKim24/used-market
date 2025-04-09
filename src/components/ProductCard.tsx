9bb0e5413746b5a3c1f0220c8f814a1fb38b3427
HarryKim24 <tl9434@naver.com>
2025-04-09 16:00:53 +0900

    ProductCard 컴포넌트 생성

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,61 @@
+'use client'
+import { Product, User } from '@prisma/client'
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React from 'react'
+import HeartButton from './HeartButton';
 
-const ProductCard = () => {
+interface ProductCardProps {
+  data: Product;
+  currentUser?: User;
+}
+
+const ProductCard = ({ data, currentUser }: ProductCardProps) => {
+
+  const router = useRouter();
+
   return (
-    <div>
-      ProductCard
+    <div 
+      onClick={() => router.push(`/products/${data.id}`)}
+      className='col-span-1 cursor-pointer group'
+    >
+      <div className='flex flex-col w-full gap-2'>
+        <div className='relative w-full overflow-hidden aspect-square rounded-xl'>
+          {
+            data.imageSrc ? 
+            <Image
+            src={data.imageSrc}
+            fill
+            sizes='auto'
+            className='object-cover w-full h-full transition group-hover:scale-110'
+            alt='product'
+          />
+          :
+          <div className='bg-gray-200 w-full h-full flex justify-center'></div>
+          }
+          <div className='absolute top-3 right-3'>
+            <HeartButton 
+              productId={data.id}
+              currentUser={currentUser}
+            />
+          </div>
+        </div>
+        <div className='text-lg font-semibold'>
+          {data.title}
+        </div>
+        <div className='font-light text-neutral-500'>
+          {data.category}
+        </div>
+        <div>
+          <div>
+            {data.price}{" "}
+            <span className='font-light'>원</span>
+          </div>
+          <div>
+            {/* {data.createdAt} */}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
