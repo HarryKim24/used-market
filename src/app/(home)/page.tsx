ad764f29d4994fc4dc82bba3e97f9ea9496a8c9e
HarryKim24 <tl9434@naver.com>
2025-04-09 14:11:02 +0900

    homepage UI 생성

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,9 @@
+import Container from "@/components/Container";
 import getProducts, { ProductsParams } from "../actions/getProducts"
+import EmptyState from "@/components/EmptyState";
+import ProductCard from "@/components/ProductCard";
+import getCurrentUser from "../actions/getCurrentUser";
+import FloatingButton from "@/components/FloatingButton";
 
 interface HomeProps {
   searchParams: ProductsParams;
@@ -7,12 +12,39 @@ interface HomeProps {
 export default async function Home({ searchParams }: HomeProps) {
 
   const products = await getProducts(searchParams);
+  const currentUser = await getCurrentUser();
 
   console.log(products);
 
   return (
-    <main>
-      누구나 볼 수 있는 페이지
-    </main>
+    <Container>
+      {
+        products?.data.length === 0
+        ?
+        <EmptyState />
+        :
+        <>
+          <div
+            className="
+              grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3
+              lg:grid-cols-4 2xl:grid-cols-6
+            "
+          >
+            {
+              products.data.map((product) => (
+                <ProductCard
+                  currentUser={currentUser}
+                  key={product.id}
+                  data={product}
+                />
+              ))
+            }
+          </div>
+        </>
+      }
+      <FloatingButton
+        href="/products/upload"
+      >+</FloatingButton>
+    </Container>
   )
 }
