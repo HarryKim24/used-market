91a28bcfcc524713adeb790b233c3dcd37963d22
HarryKim24 <tl9434@naver.com>
2025-04-16 14:34:24 +0900

    세션 데이터에 토큰 데이터 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -37,6 +37,15 @@ export const authOptions: NextAuthOptions = {
   ],
   session: {
     strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user }
+    },
+    async session({ session, token }) {
+      session.user = token;
+      return session;
+    }
   }
 };
 
