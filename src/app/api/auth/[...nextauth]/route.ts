41ca8e3001626dd6d6f0c888fbb4b9d394d15321
HarryKim24 <tl9434@naver.com>
2025-04-16 13:46:12 +0900

    Mongodb 연동 및 auth/route 작성

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,45 @@
+// app/api/auth/[...nextauth]/route.ts
+
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../../../lib/mongodb";
+
+export const authOptions: NextAuthOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (
+          credentials?.username === "test@naver.com" &&
+          credentials?.password === "test1234"
+        ) {
+          return {
+            id: "1",
+            name: "J Smith",
+            email: "jsmith@example.com",
+          };
+        }
+
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  }
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
