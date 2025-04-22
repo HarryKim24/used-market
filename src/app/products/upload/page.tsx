fc51673bee82b4c28decc8c34585998eb44d8a98
HarryKim24 <tl9434@naver.com>
2025-04-22 20:56:30 +0900

    엽로드 상품 데이터베이스에 저장

--- src/app/products/upload/page.tsx
+++ src/app/products/upload/page.tsx
@@ -5,12 +5,15 @@ import CategoryInput from '@/components/categories/CategoryInput';
 import Container from '@/components/Container';
 import ImageUpload from '@/components/ImageUpload';
 import Input from '@/components/Input';
+import axios from 'axios';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 const ProductUploadPage = () => {
 
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -45,7 +48,17 @@ const ProductUploadPage = () => {
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-
+    setIsLoading(true);
+    axios.post('/api/products', data)
+      .then(response => {
+        router.push(`/products/${response.data.id}`);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
   }
 
   const setCustomValue = (id: string, value: any) => {
