8bd8cca407d57908a3021ee3b96e79a932dc9abf
HarryKim24 <tl9434@naver.com>
2025-05-01 15:36:34 +0900

    Message 컴포넌트 구현

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Avatar from '../Avatar';
+import { fromNow } from '@/helpers/dayjs';
 
 interface MessageProps {
   isSender: boolean;
@@ -19,17 +20,28 @@ const Message = ({
   console.log('isSender', isSender);
 
   return (
-    <div>
+    <div
+      className={`grid w-full grid-cols-[40px_1fr] gap-3 mx-auto`}
+      style={{ direction: `${isSender ? 'rtl' : 'ltr'}` }}
+    >
       <div>
         <Avatar src={senderImage && isSender ? senderImage : receiverImage} />
       </div>
-      <div>
-        <div>
-          <span>{isSender ? 'You' : receiverName}</span>
-          <span>
-
+      <div className='flex flex-col items-start justify-center'>
+        <div className='flex items-center gap-2 mb-2 text-sm'>
+          <span className='font-medium'>{isSender ? '나' : receiverName}</span>
+          <span className='text-xs text-gray-500 opacity-80'>
+            {fromNow(time)}
           </span>
         </div>
+        {messageText && (
+          <div className={`
+            p-2 break-all text-white rounded-lg
+            ${isSender ? "bg-[#0071e3] rounded-tr-none" : "bg-gray-400 rounded-tl-none"}
+          `}>
+            <p>{messageText}</p>
+          </div>
+        )}
       </div>
     </div>
   )
